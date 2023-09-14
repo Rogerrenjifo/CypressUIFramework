a843a9dd95720155ccab7144c1f3b955bf5c4b57
Roger Renjifo <rrrenjifo@gmail.com>
2023-09-13 23:38:46 -0400

    implement methods and elements for home page

---
+++ cypress/pages/home/home.data.js
@@ -0,0 +1,9 @@
+export class HomeData{
+  static get phone(){
+    return {
+      SamsungGalaxyS6: 'Samsung galaxy s6',
+      Iphone632gb: 'Iphone 6 32gb',
+      HTCOneM9: 'HTC One M9'
+    }
+  }
+}
---
+++ cypress/pages/home/home.methods.js
@@ -0,0 +1,21 @@
+import { HomeElements } from "./home.elements";
+
+
+export class HomeMethods{
+  static clickOnPhonesOption(){
+    HomeElements.categoriesMenu.phone.click()
+  }
+
+  static clickOnLaptopsOption(){
+    HomeElements.categoriesMenu.laptops.click()
+  }
+
+  static clickOnMonitorsOption(){
+    HomeElements.categoriesMenu.monitors.click()
+  }
+
+  static clickOnProductLink(productName){
+    HomeElements.product(productName).click()
+  }
+  
+}
