f6ea899cd8ef060c61a6c117750e954e4b55d1bb
Roger Renjifo <rrrenjifo@gmail.com>
2023-09-13 22:41:28 -0400

    implement sign up page, methods and elements

---
+++ cypress/pages/login/signup/signup.elements.js
@@ -0,0 +1,22 @@
+export class SignUpElements {
+    static get textboxes(){
+      return {
+        get username() {
+          return cy.get("#sign-username")
+        },
+        get password() {
+          return cy.get("#sign-password")
+        }
+      }
+    }
+    static get buttons() {
+      return {
+        get close() {
+            return cy.contains("button", "Close")
+        },
+        get login() {
+          return cy.contains("button", "Log in")
+        }
+      }
+    }
+  }
